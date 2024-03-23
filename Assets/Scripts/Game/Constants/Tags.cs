26c3e135ba28596a609681314103f6f6c8cc18a5
Simone Locatelli <locatelli.simone.1993@gmail.com>
2024-03-23 19:29:49 +0000

    Added sounds for sword hitting Skeleton. Added Skeleton dead animation and sounds, with lifebar disappearing when NPC is dead

--- Assets/Scripts/Game/Constants/Tags.cs
+++ Assets/Scripts/Game/Constants/Tags.cs
@@ -1,11 +1,19 @@
 ﻿public static class Tags
 {
+    // Materials
     public static readonly string Material_Wood = "Material_Wood";
+    public static readonly string Material_Bone = "Material_Bone";
 
     public static readonly string Object = "Object";
 
+    public static readonly string NPC = "NPC";
+
     public static class Collections
     {
-        public static readonly string[] Object_Wood = new[] { Tags.Object, Tags.Material_Wood };
+        // Objects
+        public static readonly string[] Object_Wood = new[] { Tags.Material_Wood,  Tags.Object  };
+
+        // NPCs
+        public static readonly string[] NPC_Skeleton = new[] { Tags.Material_Bone,  Tags.NPC };
     }
 }
--- Assets/Scripts/Game/NPCs/EnemyController.cs
+++ Assets/Scripts/Game/NPCs/EnemyController.cs
@@ -21,6 +21,7 @@ public class EnemyController : BaseBehaviour3D
     [SerializeField]
     private bool canFollowTarget;
 
+
     public NavMeshAgent Agent => GetInitialisedComponent(ref _agent);
 
     [ReadOnlyProperty]
@@ -50,8 +51,28 @@ public class EnemyController : BaseBehaviour3D
     [ReadOnlyProperty]
     [SerializeField] private Vector3 rbVelocity;
 
+
+    private LifeSystem _lifeSystem;
+    private LifeSystem LifeSystem => GetInitialisedComponent(ref _lifeSystem);
+
+    private void Start()
+    {
+        LifeSystem.OnDeath += OnDeath;
+    }
+
+    private void OnDeath(LifeSystem lifeSystem)
+    {
+        Animator.SetBool("IsDying", true);
+        Agent.destination = transform.position;
+    }
+
     private void Update()
     {
+        if (LifeSystem.IsDead)
+             return;
+
+
+
         Agent.stoppingDistance = stoppingDistance;
         //Agent.speed = speed;
         if (canFollowTarget)
@@ -62,7 +83,6 @@ public class EnemyController : BaseBehaviour3D
         hasPath = Agent.hasPath;
         isStopped = Agent.velocity == Vector3.zero;
 
-
         velocity = Agent.velocity;
         velocityNormalized = Agent.velocity.normalized;
         pathPending = Agent.pathPending;
@@ -70,10 +90,9 @@ public class EnemyController : BaseBehaviour3D
         pathStatus = Agent.pathStatus;
         pathEndPosition = Agent.pathEndPosition;
 
-
         if (isStopped)
             transform.LookAt(pathEndPosition);
-       
+
         Animator.SetBool("IsMoving", !isStopped);
     }
 }
